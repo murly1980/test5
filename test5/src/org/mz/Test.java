5dbedcad0222526d69dd06477900c8f5247a9a99
murly1980 <52519730+murly1980@users.noreply.github.com>
2019-07-05 19:33:47 +0530

    Update Test.java

--- test5/src/org/mz/Test.java
+++ test5/src/org/mz/Test.java
@@ -5,7 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			System.out.println("hello yo yo");
-			System.out.println("git par change kar ke dekha tha");
+			System.out.println("git par change kar ke dekha tha ");
 	}
 
 }
